a58c6c87d184ec8e0511accd8722e1951bc2d63f
Giovanni Moscato <gmoscato@student.ethz.ch>
2015-10-06 21:19:52 +0200

    Primo commit

---
+++ src/com/girildo/programminoAPI/LogicaProgramma.java
@@ -0,0 +1,198 @@
+package com.girildo.programminoAPI;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.groups.discuss.GroupDiscussInterface;
+import com.flickr4java.flickr.groups.discuss.Reply;
+import com.flickr4java.flickr.groups.discuss.ReplyObject;
+import com.girildo.programminoAPI.Messaggio.FlagMessaggio;
+
+
+
+public class LogicaProgramma
+{
+	private final static String KEY = "00b9cc2a3bf5e2896905d1fd621a20eb";
+	private final static String SECRET = "a5be5f21bd03edc2";
+	
+	public LogicaProgramma()
+	{
+		Commento.resetVotingFlag();
+	}
+	
+	public Messaggio caricaDaLink(String url)
+	{
+		Pattern pattern = Pattern.compile(".+\\/discuss\\/");
+		Matcher matcher = pattern.matcher(url);
+		String idSporco = matcher.replaceAll("");
+		String id = idSporco.endsWith("/")?idSporco.substring(0, idSporco.length()-1):idSporco;
+		try
+		{
+			return new Messaggio(ottieniCommenti(id), FlagMessaggio.NESSUN_ERRORE);
+		}
+		catch(FlickrException ex)
+		{
+			if(ex.getErrorCode().equalsIgnoreCase("1"))
+				return new Messaggio("Non ho trovato un topic di Flickr valido a quel link."
+						+ "\nAssicurati di aver copiato l'URL per intero", FlagMessaggio.ERRORE);
+			return new Messaggio(null, FlagMessaggio.ERRORE);
+		}
+		catch(IllegalArgumentException ex)
+		{
+			return new Messaggio(ex.getMessage(), FlagMessaggio.ERRORE);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	ArrayList<Commento> listaCommenti;
+	HashMap<Integer, Foto> dictionaryFoto;
+	
+	private String ottieniCommenti(String topicID) throws FlickrException
+	{
+		Flickr flickr = new Flickr(KEY, SECRET, new REST());
+        GroupDiscussInterface dInterface = flickr.getDiscussionInterface();
+        int count = dInterface.getTopicInfo(topicID).getCountReplies(); //count delle risposte
+        ReplyObject rep = dInterface.getReplyList(topicID, count, 1); //ottiene l'oggetto dall'API
+        ArrayList<Reply> repList = rep.getReplyList(); //estrae la lista delle risposte
+        listaCommenti = new ArrayList<Commento>(); //lista di commenti con classe custom
+        for(Reply reply : repList)
+        {
+        	Pattern p = Pattern.compile("<img class.+alt=\"Classifica\" />");
+        	if(p.matcher(reply.getMessage()).find())
+        	{
+        		//System.out.println("qui");
+        		break;
+        	}
+        	Commento commento = new Commento(reply.getMessage(), new Autore(reply.getAuthorname(), reply.getAuthorId()));
+        	listaCommenti.add(commento);
+        }
+        StringBuilder builder = new StringBuilder();
+        for(Commento c:listaCommenti)
+        {
+        	if(c.getTipo() == Commento.TipoCommento.FOTO)
+        		builder.append(c.toString()+'\n');
+        }
+        return builder.toString();
+	}
+
+	List<Autore> listaAutoVoto, listaAutoriCheHannoVotato;
+	
+	public Messaggio generaClassifica(int numPreferenze)
+	{
+		dictionaryFoto = new HashMap<Integer, Foto>();
+		listaAutoVoto = new ArrayList<Autore>();
+		listaAutoriCheHannoVotato = new ArrayList<Autore>();
+		
+		ArrayList<Commento> commentiConErrori = new ArrayList<Commento>(); //lista che contiene i commenti con meno preferenze dell'imp.
+		for(Commento c:listaCommenti)
+		{
+			if(c.getTipo() == Commento.TipoCommento.STARTVOTING || c.getTipo() == Commento.TipoCommento.IGNORA)
+				continue;
+			if(c.getTipo() == Commento.TipoCommento.VOTAZIONE)
+			{
+				String[] split = c.getTesto().split("#", -1);
+				if(split.length<numPreferenze+1) //Se ho meno di quante preferenze mi aspetto c'è un problema
+				{
+					commentiConErrori.add(c);
+					return new Messaggio("Il voto di " + c.getAutore().getNome() + " sembra avere un problema col "+
+					"numero delle preferenze", FlagMessaggio.ERRORE);
+				}
+				ArrayList<Foto> fotoVotateInQuestoCommento = new ArrayList<Foto>();
+				for(int i=0; i<numPreferenze; i++) //itera tra gli id ottenuti con lo split 
+				{
+					int id = 0;
+					Foto foto = null;
+					try
+					{
+						id = Integer.parseInt(split[i+1]); //cast a int della stringa
+						foto = dictionaryFoto.get(id); //ottiene la foto dall'hash set
+						if(!fotoVotateInQuestoCommento.contains(foto))
+						{
+							foto.aumentaVoti(numPreferenze-i); //aumenta i voti della foto
+							fotoVotateInQuestoCommento.add(foto);
+						}
+						else
+						{
+							commentiConErrori.add(c);
+							return new Messaggio("Il commento di " + c.getAutore().getNome() + " contiene un doppio voto",
+									FlagMessaggio.ERRORE);
+						}
+						
+						if(!listaAutoriCheHannoVotato.contains(c.getAutore()))
+							listaAutoriCheHannoVotato.add(c.getAutore());
+						
+						if(foto.getAutore().equals(c.getAutore())) //se l'autore della foto è lo stesso del commenot
+						{
+							listaAutoVoto.add(c.getAutore()); //c'è autovoto
+						}
+					}
+					catch(Exception e)
+					{
+						System.out.println(id);
+					}
+				}
+			}
+			else //se non è votazione (=È foto)
+			{
+				Autore autore = c.getAutore();
+				int id = Integer.parseInt(c.getTesto().replace("#", ""));
+				Foto foto = new Foto(autore, id);
+				autore.setSuaFoto(foto);
+				dictionaryFoto.put(id, foto);
+			}
+		}
+		List<Foto> listaOrdinataPerClassifica = new ArrayList<Foto>(dictionaryFoto.values());
+		Collections.sort(listaOrdinataPerClassifica); //ordina la lista (dal più basso al più alto)
+		Collections.reverse(listaOrdinataPerClassifica); //inverte l'ordine
+		StringBuilder builderClassifica = new StringBuilder();
+		StringBuilder builderNonVotanti = new StringBuilder("Non hanno votato: \n");
+		StringBuilder builderAutoVoto = new StringBuilder("Si sono autovotati: \n");
+		
+		FlagMessaggio flagXMessaggio = FlagMessaggio.NESSUN_ERRORE;
+		
+		for(Foto f:listaOrdinataPerClassifica)
+		{
+			if(!listaAutoriCheHannoVotato.contains(f.getAutore()))
+			{
+				builderNonVotanti.append("• " + f.getAutore().getNomeAbbreviato()+
+						" (#"+f.getID()+" con "+f.getVoti()+" punti)\n");
+				flagXMessaggio = FlagMessaggio.ERRORE_PARZIALE;
+			}
+			if(listaAutoVoto.contains(f.getAutore()))
+			{
+				builderAutoVoto.append("• " + f.getAutore().getNomeAbbreviato()+"\n");
+				flagXMessaggio = FlagMessaggio.ERRORE_PARZIALE;
+			}
+			builderClassifica.append(f.toString() + "\n");
+		}
+		builderNonVotanti.append("-----------------------\n");
+		builderAutoVoto.append("-----------------------\n");
+		builderClassifica.append("-----------------------\n");
+		builderClassifica.append("Foto trovate: "+dictionaryFoto.size()+"\n");
+		builderClassifica.append("Hanno votato in "+listaAutoriCheHannoVotato.size()+"\n");
+		builderClassifica.append("Si sono autovotati in "+listaAutoVoto.size()+"\n");
+		builderClassifica.append("Voti con errore: "+commentiConErrori.size()+"\n");
+		
+		if(listaAutoVoto.size() == 0)
+			builderAutoVoto.delete(0, builderAutoVoto.length());
+		
+		
+		if(Collections.max(listaOrdinataPerClassifica).getVoti() == 0)
+			return new Messaggio("Sembra che tutte le foto abbiano 0 voti; controlla che il numero delle"
+					+ " preferenze di voto sia giusto!", FlagMessaggio.ERRORE);
+		
+		return new Messaggio(builderClassifica.toString(), flagXMessaggio
+				, builderNonVotanti.toString()+builderAutoVoto.toString());
+	}
+}
---
+++ src/com/girildo/programminoAPI/MainWindow.java
@@ -0,0 +1,403 @@
+package com.girildo.programminoAPI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import com.girildo.programminoAPI.Messaggio.FlagMessaggio;
+
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+public class MainWindow
+{
+
+	private JFrame frmProgramminoSoniagallery;
+	private JTextField textFieldLink;
+	private JLabel lblLink;
+	private LogicaProgramma logica;
+	private JPopupMenu popupMenu;
+	private JMenuItem mntmIncolla;
+	private JPanel panel_1;
+	private Box verticalBox;
+	private Box verticalBox_1;
+	private Box verticalBox_2;
+	private JScrollPane scrollPane;
+	private JTextArea textAreaFoto;
+	private Box verticalBox_3;
+	private JScrollPane scrollPane_2;
+	private JTextArea textAreaErrori;
+	private Box horizontalBox;
+	private JLabel lblNewLabel;
+	private JSlider slider;
+	private JButton btnGeneraClassifica;
+	private JTextArea textAreaClassifica;
+	private JScrollPane scrollPaneClassifica;
+	private JPopupMenu popupMenu_1;
+	private JMenuItem menuItemCopia;
+	private JPopupMenu popupMenu_2;
+	private JMenuItem mntmCopiaPerErrori;
+	/**
+	 * Launch the application.
+	 */
+	
+	private static String VERSIONE = "1.2 (18.5.2015)";
+	private JPopupMenu popupMenu_3;
+	private JMenuItem menuItemVersione;
+	
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try {
+					MainWindow window = new MainWindow();
+					window.frmProgramminoSoniagallery.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frmProgramminoSoniagallery = new JFrame();
+		frmProgramminoSoniagallery.setTitle("Programmino SoniaGallery");
+		frmProgramminoSoniagallery.setMinimumSize(new Dimension(800, 500));
+		//BorderLayout borderLayout = (BorderLayout) frame.getContentPane().getLayout();
+		frmProgramminoSoniagallery.setBounds(100, 100, 806, 522);
+		frmProgramminoSoniagallery.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(6, 6, 6, 6));
+		frmProgramminoSoniagallery.getContentPane().add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] {0, 689, 117, 117, 0};
+		gbl_panel.rowHeights = new int[] {29};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0};
+		panel.setLayout(gbl_panel);
+
+		lblLink = new JLabel("Link:");
+		lblLink.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_lblLink = new GridBagConstraints();
+		gbc_lblLink.insets = new Insets(0, 0, 0, 5);
+		gbc_lblLink.anchor = GridBagConstraints.EAST;
+		gbc_lblLink.gridx = 0;
+		gbc_lblLink.gridy = 0;
+		panel.add(lblLink, gbc_lblLink);
+
+		textFieldLink = new JTextField();
+		
+		popupMenu = new JPopupMenu();
+		addPopup(textFieldLink, popupMenu);
+		
+		mntmIncolla = new JMenuItem("Incolla");
+		mntmIncolla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				incolla();
+			}
+		});
+		popupMenu.add(mntmIncolla);
+		GridBagConstraints gbc_textFieldLink = new GridBagConstraints();
+		gbc_textFieldLink.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldLink.anchor = GridBagConstraints.NORTH;
+		gbc_textFieldLink.insets = new Insets(0, 0, 0, 5);
+		gbc_textFieldLink.gridx = 1;
+		gbc_textFieldLink.gridy = 0;
+		panel.add(textFieldLink, gbc_textFieldLink);
+
+		JButton btnOttieniCommenti = new JButton("Ottieni commenti");
+		btnOttieniCommenti.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				ottieniCommenti(textFieldLink.getText());
+			}
+		});
+		btnOttieniCommenti.setHorizontalAlignment(SwingConstants.LEADING);
+		GridBagConstraints gbc_btnOttieniCommenti = new GridBagConstraints();
+		gbc_btnOttieniCommenti.insets = new Insets(0, 0, 0, 5);
+		gbc_btnOttieniCommenti.anchor = GridBagConstraints.NORTHEAST;
+		gbc_btnOttieniCommenti.gridx = 2;
+		gbc_btnOttieniCommenti.gridy = 0;
+		panel.add(btnOttieniCommenti, gbc_btnOttieniCommenti);
+		
+		btnGeneraClassifica = new JButton("Genera classifica");
+		
+		btnGeneraClassifica.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				generaClassifica();
+			}
+		});
+		btnGeneraClassifica.setEnabled(false);
+		GridBagConstraints gbc_btnGeneraClassifica = new GridBagConstraints();
+		gbc_btnGeneraClassifica.gridx = 3;
+		gbc_btnGeneraClassifica.gridy = 0;
+		panel.add(btnGeneraClassifica, gbc_btnGeneraClassifica);
+		
+		panel_1 = new JPanel();
+		frmProgramminoSoniagallery.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new MigLayout("", "[grow,fill][grow,fill][grow,fill]", "[grow,fill]"));
+		
+		verticalBox = Box.createVerticalBox();
+		verticalBox.setBorder(new TitledBorder(null, "Foto trovate", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.add(verticalBox, "cell 0 0,width 33%,grow");
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+		verticalBox.add(scrollPane);
+		
+		textAreaFoto = new JTextArea();
+		textAreaFoto.setEditable(false);
+		scrollPane.setViewportView(textAreaFoto);
+		
+		verticalBox_1 = Box.createVerticalBox();
+		verticalBox_1.setBorder(new TitledBorder(null, "Classifica", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.add(verticalBox_1, "cell 1 0,width 33%,grow");
+		
+		scrollPaneClassifica = new JScrollPane();
+		scrollPaneClassifica.setAlignmentX(Component.LEFT_ALIGNMENT);
+		verticalBox_1.add(scrollPaneClassifica);
+		
+		textAreaClassifica = new JTextArea();
+		
+		textAreaClassifica.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) 
+			{
+//				textAreaClassifica.setSelectionStart(0);
+//				textAreaClassifica.setSelectionEnd(textAreaClassifica.getText().length());
+				textAreaClassifica.selectAll();
+			}
+			
+		});
+		
+		
+		textAreaClassifica.setEditable(false);
+		scrollPaneClassifica.setViewportView(textAreaClassifica);
+		
+		popupMenu_1 = new JPopupMenu();
+		addPopup(textAreaClassifica, popupMenu_1);
+		
+		menuItemCopia = new JMenuItem("Copia");
+		menuItemCopia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				copia(textAreaClassifica.getSelectedText());
+			}
+		});
+		menuItemCopia.setEnabled(false);
+		popupMenu_1.add(menuItemCopia);
+		
+		verticalBox_2 = Box.createVerticalBox();
+		verticalBox_2.setBorder(null);
+		panel_1.add(verticalBox_2, "cell 2 0,width 33%,grow");
+		
+		horizontalBox = Box.createHorizontalBox();
+		horizontalBox.setBorder(new TitledBorder(null, "Impostazioni", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		verticalBox_2.add(horizontalBox);
+		
+		lblNewLabel = new JLabel("# Voti");
+		horizontalBox.add(lblNewLabel);
+		
+		slider = new JSlider();
+		slider.setSnapToTicks(true);
+		slider.setPaintTicks(true);
+		slider.setMaximum(10);
+		slider.setValue(4);
+		slider.setPaintLabels(true);
+		slider.setMajorTickSpacing(1);
+		horizontalBox.add(slider);
+		
+		verticalBox_3 = Box.createVerticalBox();
+		verticalBox_3.setBorder(new TitledBorder(null, "Errori", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		verticalBox_2.add(verticalBox_3);
+		
+		scrollPane_2 = new JScrollPane();
+		verticalBox_3.add(scrollPane_2);
+		
+		textAreaErrori = new JTextArea();
+		textAreaErrori.setForeground(new Color(204, 0, 0));
+		textAreaErrori.setWrapStyleWord(true);
+		textAreaErrori.setLineWrap(true);
+		textAreaErrori.setEditable(false);
+		scrollPane_2.setViewportView(textAreaErrori);
+		
+		popupMenu_2 = new JPopupMenu();
+		addPopup(textAreaErrori, popupMenu_2);
+		
+		mntmCopiaPerErrori = new JMenuItem("Copia");
+		mntmCopiaPerErrori.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				copia(textAreaErrori.getText());
+			}
+		});
+		popupMenu_2.add(mntmCopiaPerErrori);
+		
+		popupMenu_3 = new JPopupMenu();
+		addPopup(frmProgramminoSoniagallery, popupMenu_3);
+		
+		menuItemVersione = new JMenuItem("New menu item");
+		menuItemVersione.setEnabled(false);
+		menuItemVersione.setText("Versione: "+VERSIONE);
+		popupMenu_3.add(menuItemVersione);
+	}
+
+	protected void generaClassifica()
+	{
+		Messaggio mess = logica.generaClassifica(this.slider.getValue());
+		if(mess.getFlag() == FlagMessaggio.NESSUN_ERRORE)
+		{
+			textAreaClassifica.setText(mess.getTestoNessunErrore());
+			scrollPaneClassifica.scrollRectToVisible(new Rectangle());
+		}
+		else if(mess.getFlag() == FlagMessaggio.ERRORE_PARZIALE)
+		{
+			textAreaClassifica.setText(mess.getTestoNessunErrore());
+			textAreaErrori.setText(mess.getTestoErroreParziale());
+			scrollPaneClassifica.scrollRectToVisible(new Rectangle());
+		}
+		else
+		{
+			textAreaErrori.setText(mess.getTestoNessunErrore());
+			textAreaClassifica.setText("");
+		}
+		this.menuItemCopia.setEnabled(this.textAreaClassifica.getText().length() != 0);
+	}
+
+	protected void ottieniCommenti(final String link)
+	{
+		logica = new LogicaProgramma();
+		this.reset();
+		
+		final DialogWait dial = new DialogWait(this.frmProgramminoSoniagallery);
+		dial.setLocationRelativeTo(this.frmProgramminoSoniagallery);
+		dial.setVisible(true);
+		
+		SwingWorker<Void, Void> task = new SwingWorker<Void,Void>()
+		{
+			@Override
+			protected Void doInBackground() throws Exception
+			{
+				Messaggio mess = logica.caricaDaLink(link);
+				if(mess.getFlag() == FlagMessaggio.NESSUN_ERRORE)
+				{
+					textAreaFoto.setText(mess.getTestoNessunErrore());
+					btnGeneraClassifica.setEnabled(true);
+				}
+				else if(mess.getFlag() == FlagMessaggio.ERRORE)
+					textAreaErrori.setText(mess.getTestoNessunErrore());
+				return null;
+			}
+			@Override
+			protected void done()
+			{
+				dial.setVisible(false);
+				dial.dispose();
+			}
+
+		};
+		task.execute();
+	}
+
+	private void reset()
+	{
+		this.textAreaClassifica.setText("");
+		this.textAreaErrori.setText("");
+		this.textAreaFoto.setText("");
+	}
+
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+	
+	private void incolla()
+	{
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		try 
+		{
+			textFieldLink.setText((String)clipboard.getContents(null).getTransferData(DataFlavor.stringFlavor));
+		} 
+		catch (UnsupportedFlavorException e1) 
+		{
+			textFieldLink.setText("Non riesco ad incollare il testo dagli appunti");
+		} 
+		catch (IOException e1) 
+		{
+			textFieldLink.setText("Non riesco ad incollare il testo dagli appunti");
+		}
+	}
+	protected void copia(String selectedText)
+	{
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		StringSelection s = new StringSelection(selectedText);
+		clipboard.setContents(s, null);
+		textAreaClassifica.setSelectionStart(0);
+		textAreaClassifica.setSelectionEnd(0);
+	}
+
+}
